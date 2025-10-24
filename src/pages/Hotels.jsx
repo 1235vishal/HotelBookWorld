26b9cccdf41e5f24eb4bf4f39fd197cb96bbeb24
1235vishal <vishalrajput15817@gmail.com>
2025-10-24 10:48:29 +0530

    Fix case-sensitive import path for Signup

--- src/pages/Hotels.jsx
+++ src/pages/Hotels.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import HotelHero from '../pages/Common/HotelHero';
 import AllRooms from './Hotels/AllRooms';
+import Footer from './Common/Footer';
 
 const Hotels = () => {
   return (
       <>
           <HotelHero />
-            <AllRooms />
+          <AllRooms />
+        <Footer />
       </>
   )
 }
